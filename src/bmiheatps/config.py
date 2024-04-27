7f88bfcf5e3049f4ef7c22d47b3dc2a65cac248a
Mark Piper <mark.piper@colorado.edu>
2024-04-27 17:40:06 -0600

    Write heat config files for study

--- src/bmiheatps/config.py
+++ src/bmiheatps/config.py
@@ -1,6 +1,12 @@
 """Set up a BmiHeat parameter study from a config file."""
 
+import numpy as np
+import pathlib
 import yaml
+
+HERE = pathlib.Path(__file__)
+ROOT = HERE.parent.parent.parent
+HEAT_CONFIG_FILE = ROOT / "tests" / "data" / "heat-config.yaml"
 
 
 def read_config_file(config_file: str) -> dict: 
@@ -15,4 +21,19 @@ def write_config_file(config: dict, config_file: str) -> None:
 
 
 class BmiHeatStudyConfig(object):
-    pass
+
+    def __init__(self, study_config_file: str) -> None:
+        self.heat_config = None
+        self.study_config_file = study_config_file
+        self.study_config = None
+        self.alpha = None
+
+    def setup_study(self) -> None:
+        self.heat_config = read_config_file(HEAT_CONFIG_FILE)
+        self.study_config = read_config_file(self.study_config_file)
+        self.alpha = np.linspace(self.study_config["alpha_start"], self.study_config["alpha_stop"], num=self.study_config["n_steps"], endpoint=True)
+
+    def generate_study(self) -> None:
+        for i in range(self.study_config["n_steps"]):
+            self.heat_config["alpha"] = float(self.alpha[i])
+            write_config_file(self.heat_config, f"heat-config-{i+1}.yaml")
