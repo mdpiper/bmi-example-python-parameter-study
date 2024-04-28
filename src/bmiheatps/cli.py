ff04d7b42e2db2cd75011b2a5b11ea2fd203c0f5
Mark Piper <mark.piper@colorado.edu>
2024-04-27 21:34:18 -0600

    Make setup and run cli subcommands

--- src/bmiheatps/cli.py
+++ src/bmiheatps/cli.py
@@ -1,11 +1,39 @@
 """Command-line interface for bmi-heat-study """
 
+import os
 import click
 
+from .config import BmiHeatStudyConfig
 from .study import BmiHeatParameterStudy
 
 
-@click.command()
+@click.group(chain=True)
+@click.version_option()
+@click.option(
+    "--cd",
+    default=".",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+    help="Change to a directory, then execute.",
+)
+def main(cd) -> None:
+    """Set up and run a parameter study on the BmiHeat model."""
+    os.chdir(cd)
+
+
+@main.command()
+@click.argument(
+    "config-file",
+    type=click.Path(exists=True),
+)
+@click.pass_context
+def setup(ctx, config_file):
+    """Generate a set of BmiHeat config files for a parameter study."""
+    setup = BmiHeatStudyConfig(config_file)
+    setup.setup_study()
+    setup.generate_study()
+
+
+@main.command()
 @click.argument(
     "config-file",
     type=click.Path(exists=True),
@@ -15,8 +43,9 @@ from .study import BmiHeatParameterStudy
     is_flag=True,
     help="Show the final temperature field.",
 )
-def main(config_file, show):
-    """Run a parameter study on the BmiHeat model."""
+@click.pass_context
+def run(ctx, config_file, show):
+    """Run the BmiHeat model for a set of parameters."""
     study = BmiHeatParameterStudy(config_file, show)
     study.run()
     if study._show:
