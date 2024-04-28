16ce3c24756cb2c2b24b6da4cca15d1f86b8bded
Mark Piper <mark.piper@colorado.edu>
2024-04-27 21:44:39 -0600

    Don't use dynamic versioning

--- src/bmiheat_parameter_study/__init__.py
+++ src/bmiheat_parameter_study/__init__.py
@@ -1,10 +1,8 @@
-from ._version import __version__
 from .config import BmiHeatStudyConfig
 from .study import BmiHeatParameterStudy
 
 
 __all__ = [
-    "__version__",
     "BmiHeatParameterStudy",
     "BmiHeatStudyConfig",
 ]
