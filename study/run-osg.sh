a82960962e33b12faf4fc622c15482cafb2ba662
Allen Lee <alee@users.noreply.github.com>
2024-05-14 08:53:08 -0700

    fix: place code in /code instead of /srv
    /srv gets remapped by the apptainer execution environment to include the
    image and the executable + all input files specified by
    transfer_input_files (is there docs on this from OSG?)

--- study/run-osg.sh
+++ study/run-osg.sh
@@ -6,7 +6,8 @@ printf "Job running on node: "; /bin/hostname
 printf "OSG site: $OSG_SITE_NAME"
 printf "Job running as user: "; /usr/bin/id
 printf "Current working directory: "; /usr/bin/pwd
-printf "contents of /srv: "; /usr/bin/ls -al /srv
+printf "contents of /srv and /code: "; /usr/bin/ls -al /srv /code
 echo "Command line args: $@"
 
-bmiheat-parameter-study run heat-config-${1}.yaml > output-${1}.txt 2>&1
+mkdir -p /srv/results
+cd /code && bmiheat-parameter-study run heat-config-${1}.yaml > /srv/results/output-${1}.txt 2>&1
